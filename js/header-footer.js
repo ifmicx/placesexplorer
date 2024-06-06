e3bbb926b8cecf3f66f23ca49251d73ea067cc2d
Michele Polini <michele_polini@outlook.com>
2024-06-06 22:16:00 +0200

    agg

--- js/header-footer.js
+++ js/header-footer.js
@@ -1,9 +1,9 @@
 function stampaHeader() {
-    document.getElementById("header").innerHTML = "<nav><div class='logo'><a href='../index.html'><img src='../img/logo.png' alt='Home' width='200' height='66'></a></div><input type='checkbox' class='toggler' /><div class='hamburger'><div></div></div><div class='menu'><button id='dark-switch' onclick='darkmode()'>🌙</button><a class='menu-item' href='../index.html'>Home</a><a class='menu-item active' href='./about.html'>Su di noi</a><a class='menu-item' href='./places.html'>Posti</a><a class='menu-item' href='./contacts.html'>Contatti</a></div></nav>";
+    document.getElementById("header").innerHTML = "<nav><div class='logo'><a href='../index.html'><img src='../img/logo.png' alt='Home' width='200' height='66'></a></div><input type='checkbox' class='toggler' /><div class='hamburger'><div></div></div><div class='menu'><button id='dark-switch' onclick='darkmode()'>🌙</button><a class='menu-item' href='../index.html'>Home</a><a class='menu-item' href='./about.html'>Su di noi</a><a class='menu-item' href='./places.html'>Posti</a><a class='menu-item' href='./contacts.html'>Contatti</a></div></nav>";
 }
 
 function stampaHeaderIndex() {
-    document.getElementById("header").innerHTML = "<nav><div class='logo'><a href='index.html'><img src='img/logo.png' alt='Home' width='200' height='66'></a></div><input type='checkbox' class='toggler' /><div class='hamburger'><div></div></div><div class='menu'><button id='dark-switch' onclick='darkmode()'>🌙</button><a class='menu-item' href='index.html'>Home</a><a class='menu-item active' href='pages/about.html'>Su di noi</a><a class='menu-item' href='pages/places.html'>Posti</a><a class='menu-item' href='pages/contacts.html'>Contatti</a></div></nav>";
+    document.getElementById("header").innerHTML = "<nav><div class='logo'><a href='index.html'><img src='img/logo.png' alt='Home' width='200' height='66'></a></div><input type='checkbox' class='toggler' /><div class='hamburger'><div></div></div><div class='menu'><button id='dark-switch' onclick='darkmode()'>🌙</button><a class='menu-item' href='index.html'>Home</a><a class='menu-item' href='pages/about.html'>Su di noi</a><a class='menu-item' href='pages/places.html'>Posti</a><a class='menu-item' href='pages/contacts.html'>Contatti</a></div></nav>";
 }
 
 function stampaFooter() {
