1fefd12a3401d826958e4d34ad5421c0c7cd8845
Michele Polini <michele_polini@outlook.com>
2024-06-06 21:55:35 +0200

    agg

--- js/header-footer.js
+++ js/header-footer.js
@@ -1,3 +1,7 @@
 function stampaHeader() {
     document.getElementById("header").innerHTML = "<nav><div class='logo'><a href='../index.html'><img src='../img/logo.png' alt='Home' width='200' height='66'></a></div><input type='checkbox' class='toggler' /><div class='hamburger'><div></div></div><div class='menu'><button id='dark-switch' onclick='darkmode()'>🌙</button><a class='menu-item' href='../index.html'>Home</a><a class='menu-item active' href='./about.html'>Su di noi</a><a class='menu-item' href='./places.html'>Posti</a><a class='menu-item' href='./contacts.html'>Contatti</a></div></nav>";
 }
+
+function stampaFooter() {
+    document.getElementById("footer").innerHTML = "<div class='footer'>© 2024 Places Explorer</div>";
+}
