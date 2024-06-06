c09db1340866018907caacc9866ff1ebdd726021
Michele Polini <michele_polini@outlook.com>
2024-06-06 19:20:00 +0200

    header

---
+++ js/header.js
@@ -0,0 +1,3 @@
+function stampaHeader() {
+    document.getElementById("header").innerHTML = "<nav><div class='logo'><a href='../index.html'><img src='../img/logo.png' alt='Home' width='200' height='66'></a></div><input type='checkbox' class='toggler' /><div class='hamburger'><div></div></div><div class='menu'><button id='dark-switch' onclick='darkmode()'>🌙</button><a class='menu-item' href='../index.html'>Home</a><a class='menu-item active' href='./about.html'>Su di noi</a><a class='menu-item' href='./places.html'>Posti</a><a class='menu-item' href='./contacts.html'>Contatti</a></div></nav>";
+}
